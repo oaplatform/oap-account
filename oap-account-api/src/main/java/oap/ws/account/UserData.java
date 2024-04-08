0a52cefae9df825d575daf9c1a21e6a5cd2ddd4f
JuliaGalabut <julia.galabut@xenoss.io>
2024-04-08 11:51:08 +0300

    fixed checkstyle

--- oap-account-api/src/main/java/oap/ws/account/UserData.java
+++ oap-account-api/src/main/java/oap/ws/account/UserData.java
@@ -188,8 +188,8 @@ public class UserData implements oap.ws.sso.User, Serializable {
     }
 
     public UserData removeAccount( String organizationId, String accountId ) {
-        final var organization = this.accounts.get(organizationId);
-        organization.remove(accountId);
+        final var organization = this.accounts.get( organizationId );
+        organization.remove( accountId );
         final var defaultAccount = this.getDefaultAccount( organizationId );
         if( defaultAccount.isPresent() && accountId.equals( defaultAccount.get() ) ) {
             this.user.defaultAccounts.remove( organizationId );
