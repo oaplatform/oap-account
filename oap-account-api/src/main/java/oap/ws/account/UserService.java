41a822656feb18402515f4b51b723f9291677732
igor.petrenko <igor.petrenko@xenoss.io>
2025-08-29 10:02:59 +0300

    add UserService

--- oap-account-api/src/main/java/oap/ws/account/UserService.java
+++ oap-account-api/src/main/java/oap/ws/account/UserService.java
@@ -1,10 +1,14 @@
 package oap.ws.account;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.List;
 
 public interface UserService {
     List<UserInfo> getInfo( String... emails );
 
-    record UserInfo( String email, String firstName, String lastName ) {
+    record UserInfo( String email, String firstName, String lastName ) implements Serializable {
+        @Serial
+        private static final long serialVersionUID = -8190806637390172209L;
     }
 }
